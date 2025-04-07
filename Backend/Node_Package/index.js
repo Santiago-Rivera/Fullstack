3efd50aa3f57b2c09ca220372e0f5225062a53a9
Santiago Rivera <sarivera.es@tecnologicoargos.edu.ec>
2025-04-06 19:40:49 -0500

    Backend (Node_Package)

---
+++ Backend/Node_Package/index.js
@@ -0,0 +1,16 @@
+//var generateName = require('sillyname');
+
+//import generateName from 'sillyname';
+
+//var sillyName = generateName();
+
+//console.log('My name is ' + sillyName + '.');
+
+//Challenge 1: Superhero Name Generator
+//Create a function that generates a superhero name using the following rules:
+
+import superheroesName from "superheroesname";
+
+var superheroesName = superheroes.random();
+
+console.log('My superhero name is ' + superheroesName + '.');
