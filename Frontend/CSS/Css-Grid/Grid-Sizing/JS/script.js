1cd2ee6389842fa9901af50bdeac7a88e3c29e10
Santiago Rivera <sarivera.es@tecnologicoargos.edu.ec>
2025-07-10 20:16:56 -0500

    Añade estructura y estilos CSS para el ejercicio de tamaño de cuadrícula

---
+++ Frontend/CSS/Css-Grid/Grid-Sizing/JS/script.js
@@ -0,0 +1,6 @@
+    function applyUserCss() {
+      const userCss = document.getElementById('user-css').value;
+      const style = document.createElement('style');
+      style.innerHTML = userCss;
+      document.head.appendChild(style);
+    }
